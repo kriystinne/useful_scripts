6a08fe1accee20d3d5e5587913fdd45b09a5668a
kriystinne <anca.vasile@protonmail.com>
2020-10-12 13:41:04 +0200

    added more links and changed codec method

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ scripts used in school to make my life easier
 ### Script 1: download rmpt videos
 In school, we had streaming videos for all our lectures. I inspired myself from [@vagnes](https://github.com/vagnes)'s script that works on windows to add the same functionality on a MAC.
 
+Output method: Raw; this is because videos that are saved with the TS method do not work on a mac. Read more [here](https://www.videolan.org/streaming-features.html).
+
+I also excuded the verbose option as it wasn't necessary.
--- rmtp_mp4.py
+++ rmtp_mp4.py
@@ -6,14 +6,19 @@ import os
 # if args is a string, the string is interpreted as the name or path of the program to execute
 
 # list for the links to download
-rmpt_list = ["rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-08-27.mp4",
-            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-08-28.mp4",
-            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-08-29.mp4",
+rmpt_list = [
             "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-03.mp4",
             "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-04.mp4",
             'rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-05.mp4',
             "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-10.mp4",
-            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-11.mp4"
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-11.mp4",
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-12.mp4",
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-17.mp4",
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-18.mp4",
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-19.mp4",
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-24.mp4",
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-25.mp4",
+            "rtmp://streaming.noroff.no:1935/vod/UC2OOP101_2018-09-26.mp4"   
 ]
 
 
@@ -32,13 +37,13 @@ def download_videos(path, video_list):
     os.chdir(path)
 
     # loop through the list of links and download them, saving with the name of the file
+    # use file/mp4 as per the documentation here https://www.videolan.org/streaming-features.html since ts doesn't work with quicktime
     for fname in rmpt_list:
-        split = fname.split("/")
-        final_name = split[4]
-        Popen(["/Applications/VLC.app/Contents/MacOS/VLC", "-v", f"{fname}",
-        "--sout", f"file/ts:{final_name}"])
+        split = fname.split("/")[4]
+        final_name = split.replace(".mp4", "",)
+        print("Working on ", fname)
+        Popen(["/Applications/VLC.app/Contents/MacOS/VLC", "-vvv", f"{fname}",
+        "--sout", f"file/mp4:{final_name}.mp4"])
 
 if __name__ == "__main__":
-    download_videos("/Volumes/T7 Touch/NOROFF/Downloaded from Moodle/Year2/Object Oriented Programming/Videos/",
-    rmpt_list)
-
+    download_videos("/Volumes/T7 Touch/NOROFF/Downloaded from Moodle/Year2/Object Oriented Programming/Videos/",rmpt_list)
